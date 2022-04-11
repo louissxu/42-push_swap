1d095cc169e6b2681d5106b2842b64ab813db4e2
Louis Xu <louis.s.xu@gmail.com>
2022-04-11 18:41:36 +0930

    feat(core): Create doubly linked list and functions for basic operations

---
+++ dlist.c
@@ -0,0 +1,212 @@
+#include "dlist.h"
+
+t_dlist	*ft_dlist_new_node(void *content)
+{
+	t_dlist *node;
+	node = malloc(sizeof (*node) * 1);
+	if (!node)
+	{
+		return NULL;
+	}
+	node->content = content;
+	node->next = NULL;
+	node->prev = NULL;
+
+	return (node);
+}
+
+void	ft_dlist_destroy_node(t_dlist *node, void(*del)(void *))
+{
+	if (!node | !del)
+	{
+		return ;
+	}
+	del(node->content);
+	free(node);
+	
+	return ;
+}
+
+void	ft_dlist_destroy_list(t_dlist *head, void(*del)(void *))
+{
+	t_dlist *next;
+
+	if (!head | !del)
+	{
+		return ;
+	}
+	while (head)
+	{
+		next = head->next;
+		ft_dlist_destroy_node(head, del);
+		head = next;
+	}
+	return ;
+}
+
+t_dlist	*ft_dlist_get_last(t_dlist *head)
+{
+	if (!head)
+	{
+		return (NULL);
+	}
+	while (head->next)
+	{
+		head = head->next;
+	}
+	return (head);
+}
+
+void	ft_dlist_remove_first(t_dlist **head, void (*del)(void *))
+{
+	t_dlist *curr;
+	t_dlist *prev;
+
+	if (!head | !del)
+	{
+		return ;
+	}
+	if (!*head)
+	{
+		return ;
+	}
+	curr = *head;
+	prev = curr;
+	curr = curr->next;
+	ft_dlist_destroy_node(prev, del);
+	if (curr == NULL)
+	{
+		*head = NULL;
+		return ;
+	}
+	curr->prev = NULL;
+	*head = curr;
+	return ;
+}
+
+void	ft_dlist_remove_last(t_dlist **head, void (*del)(void *))
+{
+	t_dlist *last;
+	t_dlist *prev;
+
+	if (!head | !del)
+	{
+		return ;
+	}
+	if (!*head)
+	{
+		return ;
+	}
+	last = ft_dlist_get_last(*head);
+	prev = last->prev;
+	ft_dlist_destroy_node(last, del);
+	if (prev == NULL)
+	{
+		*head == NULL;
+		return ;
+	}
+	prev->next = NULL;
+	return ;
+}
+
+void	ft_dlist_insert_start(t_dlist **head, t_dlist *new_node)
+{
+	if (!head | !new_node)
+	{
+		return ;
+	}
+	if (!*head)
+	{
+		new_node->next = NULL;
+		new_node->prev = NULL;
+		*head = new_node;
+		return ;
+	}
+	new_node->next = *head;
+	new_node->prev = NULL;
+	(*head)->prev = new_node;
+	*head = new_node;
+	return ;
+}
+
+void	ft_dlist_insert_end(t_dlist **head, t_dlist *new_node)
+{
+	t_dlist *last;
+	
+	if (!head | !new_node)
+	{
+		return ;
+	}
+	if (!*head)
+	{
+		new_node->next = NULL;
+		new_node->prev = NULL;
+		*head = new_node;
+		return ;
+	}
+	last = ft_dlist_get_last(*head)
+	last->next = new_node;
+	new_node->next = NULL;
+	new_node->prev = last;
+	return ;
+}
+
+t_dlist	*ft_dlist_pop_first(t_dlist **head)
+{
+	t_dlist *node;
+
+	if (!head)
+	{
+		return (NULL);
+	}
+	if (!*head)
+	{
+		return (NULL);
+	}
+	node = *head;
+	*head = (*head)->next;
+	node->next = NULL;
+	if (*head)
+	{
+		(*head)->prev = NULL;
+	}
+	return (node);
+}
+
+t_dlist *ft_dlist_pop_last(t_dlist **head)
+{
+	t_dlist	*node;
+	
+	if (!head)
+	{
+		return (NULL);
+	}
+	if (!*head)
+	{
+		return (NULL);
+	}
+	node = ft_dlist_get_last(*head);
+	if (node->prev == NULL)
+	{
+		*head = NULL;
+	}
+	else 
+	{
+		node->prev->next = NULL;
+	}
+	node->prev = NULL;
+	return node;
+}
+
+size_t	ft_dlist_length(t_dlist *head)
+{
+	size_t	len;
+	
+	len = 0;
+	while (head)
+	{
+		len++;
+		head = head->next;
+	}
+	return (len);
+}
---
+++ dlist.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+typedef struct s_dlist {
+	void			*content;
+	struct s_dlist	*next;
+	struct s_dlist	*prev;
+}	t_dlist;
+
+t_dlist	*ft_dlist_new_node(void *content); 
+void	ft_dlist_destroy_node(t_dlist *node, void (*del)(void *));
+void	ft_dlist_destroy_list(t_dlist *head, void (*del)(void *)); 
+t_dlist *ft_dlist_get_last(t_dlist *head);
+void	ft_dlist_remove_first(t_dlist **head, void (*del)(void *));
+void	ft_dlist_remove_last(t_dlist **head, void (*del)(void *));
+void	ft_dlist_insert_start(t_dlist **head, t_dlist *new_node);
+void	ft_dlist_insert_end(t_dlist **head, t_dlist *new_node);
+t_dlist *ft_dlist_pop_first(t_dlist **head);
+t_dlist *ft_dlist_pop_last(t_dlist **head);
+size_t	ft_dlist_length(t_dlist *head);
