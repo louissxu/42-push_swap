40718e913f44d1d9051720752b912d963c49a39b
louissxu <louis.s.xu@gmail.com>
2022-07-02 00:36:22 +0930

    fix: Fix double radix sort typos - use ft math functions rather than inbuilt

--- src/ps_sort_double_radix_sort.c
+++ src/ps_sort_double_radix_sort.c
@@ -1,4 +1,4 @@
-#include "push_swap.c"
+#include "push_swap.h"
 
 int	calculate_max_radix(int num)
 {
@@ -9,10 +9,11 @@ int	calculate_max_radix(int num)
 	{
 		return 0;
 	}
-	while (num > pow(2, max_radix))
+	while (num > ft_pow(2, max_radix))
 	{
 		max_radix++;
 	}
+	return (max_radix);
 }
 
 void	ps_sort_double_radix_sort(t_deque *l, t_deque *r, t_list **moves)
@@ -31,7 +32,7 @@ void	ps_sort_double_radix_sort(t_deque *l, t_deque *r, t_list **moves)
 		i = 0;
 		while (i < max_i)
 		{
-			if (*(int *)(l->head->content) / pow(2, radix) % 2  == 0)
+			if (*(int *)(l->head->content) / ft_pow(2, radix) % 2  == 0)
 			{
 				ps_pb(l, r, moves);
 			}
@@ -39,16 +40,17 @@ void	ps_sort_double_radix_sort(t_deque *l, t_deque *r, t_list **moves)
 			{
 				ps_ra(l, r, moves);
 			}
+			i++;
 		}
 		radix++;
 		if (radix < max_radix)
 		{
 			r_first_buffer_length = ft_deque_length(*r);
 			max_i = ft_deque_length(*l);
-			i = 0
+			i = 0;
 			while (i < max_i)
 			{
-				if (*(int *)(l->head->content) / pow(2, radix) % 2  == 0)
+				if (*(int *)(l->head->content) / ft_pow(2, radix) % 2  == 0)
 				{
 					ps_pb(l, r, moves);
 				}
@@ -56,11 +58,36 @@ void	ps_sort_double_radix_sort(t_deque *l, t_deque *r, t_list **moves)
 				{
 					ps_ra(l, r, moves);
 				}
+				i++;
+			}
+			i = 0;
+			max_i = ft_deque_length(*r) - r_first_buffer_length;
+			while (i < max_i)
+			{
+				ps_rb(l, r, moves);
+				i++;
+			}
+			i = 0;
+			while (i < r_first_buffer_length)
+			{
+				if (*(int *)(r->head->content) / ft_pow(2, radix) % 2 != 0)
+				{
+					ps_pa(l, r, moves);
+				}
+				else
+				{
+					ps_rb(l, r, moves);
+				}
+				i++;
 			}
 		}
-		
-		
-
+		i = 0;
+		max_i = ft_deque_length(*r);
+		while (i < max_i)
+		{
+			ps_pa(l, r, moves);
+			i++;
+		}
+		radix += 1;
 	}
-
 }
