21db8122d58b14da9ff3fc79e9d2394001a080b7
louissxu <louis.s.xu@gmail.com>
2022-07-07 15:11:09 +0930

    fix: Fix remove duplicates. Incorrect strncmp length and popping the correct number of nodes

--- src/ps_remove_duplicates.c
+++ src/ps_remove_duplicates.c
@@ -34,7 +34,10 @@ t_dlist	*ps_remove_duplicates(t_list *l)
 	t_dlist	*dlist_head;
 	t_dlist	*node;
 	t_dlist	*prev_node;
+	t_dlist	*popped_node;
 
+
+	UNUSED (prev_node);
 	dlist_head = list_to_dlist(l);
 	node = dlist_head;
 	if (!node)
@@ -49,14 +52,16 @@ t_dlist	*ps_remove_duplicates(t_list *l)
 	{
 		if ((ft_strncmp(node->content, "pb", 3) == 0 && ft_strncmp(node->next->content, "pa", 3) == 0) || \
 			(ft_strncmp(node->content, "pa", 3) == 0 && ft_strncmp(node->next->content, "pb", 3) == 0) || \
-			(ft_strncmp(node->content, "ra", 3) == 0 && ft_strncmp(node->next->content, "rra", 3) == 0) || \
-			(ft_strncmp(node->content, "rra", 3) == 0 && ft_strncmp(node->next->content, "ra", 3) == 0) || \
-			(ft_strncmp(node->content, "rb", 3) == 0 && ft_strncmp(node->next->content, "rrb", 3) == 0) || \
-			(ft_strncmp(node->content, "rrb", 3) == 0 && ft_strncmp(node->next->content, "rb", 3) == 0))
+			(ft_strncmp(node->content, "ra", 3) == 0 && ft_strncmp(node->next->content, "rra", 4) == 0) || \
+			(ft_strncmp(node->content, "rra", 4) == 0 && ft_strncmp(node->next->content, "ra", 3) == 0) || \
+			(ft_strncmp(node->content, "rb", 3) == 0 && ft_strncmp(node->next->content, "rrb", 4) == 0) || \
+			(ft_strncmp(node->content, "rrb", 4) == 0 && ft_strncmp(node->next->content, "rb", 3) == 0))
 		{
 			prev_node = node->prev;
-			ft_dlist_pop_at_pointer(&dlist_head, node);
-			ft_dlist_destroy_node(node, free);
+			popped_node = ft_dlist_pop_at_pointer(&dlist_head, node->next);
+			ft_dlist_destroy_node(popped_node, free);
+			popped_node = ft_dlist_pop_at_pointer(&dlist_head, node);
+			ft_dlist_destroy_node(popped_node, free);
 			if (prev_node)
 			{
 				node = prev_node;
