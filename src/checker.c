50d4704fae0610186a900964465d21a7fb2547a3
Louis Xu <louis.s.xu@gmail.com>
2022-07-16 20:38:46 +0930

    feat(core): Make checker work with t_ps_data struct

--- src/checker.c
+++ src/checker.c
@@ -2,30 +2,28 @@
 
 int main(int argc, char** argv)
 {
-	t_deque	l;
-	t_deque	r;
-	t_dlist	*commands;
-	bool	err;
-	char	*new_line;
-	t_dlist	*node;
-	t_list	*bin_moves;
+	t_ps_data	d;
+	t_dlist		*commands;
+	bool		err;
+	char		*new_line;
+	t_dlist		*node;
 
 	if (argc == 1)
 	{
 		return (0);
 	}
 	err = false;
-	l = parse_input_args_to_deque(argv, &err);
+	d.l = parse_input_args_to_deque(argv, &err);
 	if (err)
 	{
-		ft_deque_destroy_list(&l, free);
+		ft_deque_destroy_list(&d.l, free);
 		write(2, "Error\n", 6);
 		return (0);
 	}
-	if (deque_has_duplicates(&l) == true)
+	if (deque_has_duplicates(&d.l) == true)
 	{
 		write(2, "Error\n", 6);
-		ft_deque_destroy_list(&l, free);
+		ft_deque_destroy_list(&d.l, free);
 		return (0);
 	}
 	commands = NULL;
@@ -40,58 +38,58 @@ int main(int argc, char** argv)
 	{
 		if (ft_strncmp(node->content, "pa\n", 4) == 0)
 		{
-			ps_pa(&l, &r, &bin_moves);
+			ps_pa(&d);
 		}
 		else if (ft_strncmp(node->content, "pb\n", 4) == 0)
 		{
-			ps_pb(&l, &r, &bin_moves);
+			ps_pb(&d);
 		}
 		else if (ft_strncmp(node->content, "ra\n", 4) == 0)
 		{
-			ps_ra(&l, &r, &bin_moves);
+			ps_ra(&d);
 		}
 		else if (ft_strncmp(node->content, "rra\n", 5) == 0)
 		{
-			ps_rra(&l, &r, &bin_moves);
+			ps_rra(&d);
 		}
 		else if (ft_strncmp(node->content, "rb\n", 4) == 0)
 		{
-			ps_rb(&l, &r, &bin_moves);
+			ps_rb(&d);
 		}
 		else if (ft_strncmp(node->content, "rrb\n", 5) == 0)
 		{
-			ps_rrb(&l, &r, &bin_moves);
+			ps_rrb(&d);
 		}
 		else if (ft_strncmp(node->content, "sa\n", 4) == 0)
 		{
-			ps_sa(&l, &r, &bin_moves);
+			ps_sa(&d);
 		}
 		else if (ft_strncmp(node->content, "sb\n", 4) == 0)
 		{
-			ps_sb(&l, &r, &bin_moves);
+			ps_sb(&d);
 		}
 		else if (ft_strncmp(node->content, "rr\n", 4) == 0)
 		{
-			ps_rr(&l, &r, &bin_moves);
+			ps_rr(&d);
 		}
 		else if (ft_strncmp(node->content, "rrr\n", 5) == 0)
 		{
-			ps_rrr(&l, &r, &bin_moves);
+			ps_rrr(&d);
 		}
 		else
 		{
 			// clean up
-			ft_deque_destroy_list(&l, free);
-			ft_deque_destroy_list(&r, free);
+			ft_deque_destroy_list(&d.l, free);
+			ft_deque_destroy_list(&d.r, free);
 			ft_dlist_destroy_list(commands, free);
-			ft_lstclear(&bin_moves, free);
+			ft_lstclear(&d.m, free);
 			// exit with error
 			ft_printf("Error\n");
 			return (0);
 		}
 		node = node->next;
 	}
-	if (deque_is_sorted(&l) == 1 && ft_deque_length(r) == 0)
+	if (deque_is_sorted(&d.l) == 1 && ft_deque_length(d.r) == 0)
 	{
 		ft_printf("OK\n");
 		return (0);
@@ -99,8 +97,8 @@ int main(int argc, char** argv)
 	ft_printf("KO\n");
 
 	//clean up
-	ft_deque_destroy_list(&l, free);
-	ft_deque_destroy_list(&r, free);
+	ft_deque_destroy_list(&d.l, free);
+	ft_deque_destroy_list(&d.r, free);
 	ft_dlist_destroy_list(commands, free);
-	ft_lstclear(&bin_moves, free);
+	ft_lstclear(&d.m, free);
 }
