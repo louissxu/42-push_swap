8a8fd6842bed1b8e53a72f675a384a0b742f3473
louissxu <louis.s.xu@gmail.com>
2022-07-17 14:59:21 +0930

    feat(core): Add python test string

--- test_values.py
+++ test_values.py
@@ -4,4 +4,4 @@ max_val = 1000;
 print(" ".join(map(str, sorted(range(0, max_val), key=lambda k: random.random()))))
 
 # Valgrind test string
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind-out.txt ./push_swap `python3 ~/Desktop/42school/42-push_swap/test_values.py`
+# valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind-out.txt ./push_swap `python3 ~/Desktop/42school/42-push_swap/test_values.py`
