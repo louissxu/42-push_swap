90111ee8a9b27cff7f4536be57450eee62f4fc1f
Louis Xu <louis.s.xu@gmail.com>
2022-08-16 18:34:12 +0930

    feat(core): Add comments documenting the struct and norm fixes

--- includes/deque.h
+++ includes/deque.h
@@ -3,12 +3,25 @@
 # include <stdlib.h>
 # include "dlist.h"
 
+/**
+ * @struct t_deque
+ * A double ended queue
+ *
+ * A double ended queue. Consists of a double ended list. Stores the pointers to
+ * the head and tail of the deque for fast (constant time) access/modification
+ * to either end of the queue.
+ *
+ * @var t_deque::head
+ *     Pointer to the head of the deque
+ * @var t_deque::tail
+ *     Pointer to the tail of the deque
+ */
 typedef struct s_deque {
 	t_dlist	*head;
 	t_dlist	*tail;
 }	t_deque;
 
-t_deque ft_deque_new();
+t_deque	ft_deque_new(void);
 void	ft_deque_destroy_list(t_deque *d, void (*del)(void *));
 void	ft_deque_append(t_deque *d, void *content);
 void	ft_deque_appendleft(t_deque *d, void *content);
