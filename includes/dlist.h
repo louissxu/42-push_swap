fe553b7283792a19b57473bb8bced5ff501a4e85
Louis Xu <louis.s.xu@gmail.com>
2022-08-16 18:42:15 +0930

    feat(core): Add comment documenting dlist struct and norm fixes

--- includes/dlist.h
+++ includes/dlist.h
@@ -2,13 +2,31 @@
 
 # include <stdlib.h>
 
+/**
+ * @struct t_dlist
+ * A double ended list
+ *
+ * A double ended list. A list with pointers to both the next and the previous
+ * nodes. Therefore, the list can be traversed (and edited) from either end.
+ * These are nodes of a dlist, when strung together forms the completed dlist.
+ * The functions typically expect to be passed the head node of a dlist.
+ *
+ * @var t_dlist::content
+ *     Void pointer to the content included in this node
+ * @var t_dlist::next
+ *     Pointer to the next s_dlist node. NULL if there is no next node (ie if
+ *     the node is the last node).
+ * @var t_dlist::prev
+ *     Pointer to the previous s_dlist node. NULL if no previous node (ie if
+ *     the node is the head node).
+ */
 typedef struct s_dlist {
 	void			*content;
 	struct s_dlist	*next;
 	struct s_dlist	*prev;
 }	t_dlist;
 
-t_dlist	*ft_dlist_new_node(void *content); 
+t_dlist	*ft_dlist_new_node(void *content);
 void	ft_dlist_destroy_node(t_dlist *node, void (*del)(void *));
 void	ft_dlist_destroy_list(t_dlist *head, void (*del)(void *));
 t_dlist	*ft_dlist_get_last(t_dlist *head);
