d294fadca3fbb488027aa5f4ec02e27ce434bda7
Louis Xu <louis.s.xu@gmail.com>
2022-08-16 18:53:45 +0930

    feat(core): Add comment documenting ps_data struct

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -9,6 +9,21 @@
 # include "libft.h"
 # include <limits.h>
 
+/**
+ * @struct t_ps_data
+ * A pushswap state. Bundle containing stack a, stack b, and a list of moves.
+ *
+ * A struct to hold the state of a sort and the moves taken to get there. Used
+ * to bundle up the two stacks and the moves list together to be passed around
+ * between functions and around the program.
+ *
+ * @var t_ps_data::a
+ *     A deque containing the current state of stack A.
+ * @var t_ps_data::b
+ *     A deque containing the current state of stack B.
+ * @var t_ps_data::m
+ *     Pointer to the head of a list containing the list of moves.
+ */
 typedef struct s_ps_data {
 	t_deque	a;
 	t_deque	b;
@@ -31,7 +46,6 @@ void	debug_print_list_of_moves(t_list *moves_node);
 void	debug_print_dlist(t_dlist *node);
 void	debug_print_deque(t_deque *d);
 
-long	ft_intcmp(const	int *i1, int *i2);
 
 void	ps_sort_2_elements(t_ps_data *d);
 void	ps_sort_3_elements(t_ps_data *d);
@@ -54,6 +68,11 @@ void	ps_selection_sort_back_with_doubles(t_ps_data *d);
 
 // PS utilities
 int		ps_find_distance_to_value(t_deque *stack, int val);
+// long	ft_intcmp(const	int *i1, int *i2);
+// t_list	*list_sort_bubble_sort(t_list *l);
+t_deque	*normalise_in_place(t_deque *d);
+bool	deque_has_duplicates(t_deque *d);
+
 
 // FT math utilities
 long	ft_math_abs(int val);
@@ -66,7 +85,6 @@ int	*clone_heap_integer(int *orig);
 void	*clone_heap_integer_void(void *orig);
 
 // Other functions - to rearrange and/or maybe rename
-t_deque	*normalise_in_place(t_deque *d);
 int	ft_pow(int x, int y);
 int	calculate_max_radix(int num);
 t_dlist	*ps_remove_duplicates(t_list *l);
@@ -74,8 +92,6 @@ void	debug_print_list_of_moves_dlist(t_dlist *d);
 
 // Shared functions needed for checker
 t_deque	parse_input_args_to_deque(char **argv, bool *err);
-bool	deque_has_duplicates(t_deque *d);
 bool	deque_is_sorted(t_deque *d);
-t_list	*list_sort_bubble_sort(t_list *l);
 
 #endif
