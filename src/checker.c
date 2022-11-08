cdb37e7870ba389201c8db2ad5a1abd9d5e392ee
Louis Xu <louis.s.xu@gmail.com>
2022-11-09 03:19:54 +1030

    feat(core): Add comments

--- src/checker.c
+++ src/checker.c
@@ -12,7 +12,19 @@
 
 #include "push_swap.h"
 
-t_dlist	*read_commands_from_stdin(void)
+/**
+ * @brief Reads in the string commands from stdin and stores them in a dlist
+ * 
+ * Reads in the value from get_next_line until the end of the stdin is reached
+ * (EOF char reached). Populates the result dlist with each command with one
+ * command per line. This will be read later by the checker.
+ * 
+ * The commands are not sanitised. At this stage, any string is accepted and
+ * placed within the dlist to be parsed later.
+ * 
+ * @return dlist populated with the commands. One line per dlist node.
+*/
+static t_dlist	*read_commands_from_stdin(void)
 {
 	t_dlist	*commands;
 	char	*new_line;
@@ -27,7 +39,16 @@ t_dlist	*read_commands_from_stdin(void)
 	return (commands);
 }
 
-void	clean_up(t_ps_data *d, t_dlist *commands)
+/**
+ * @brief Clean up. Refactored out of the main due to function line limit
+ * 
+ * Does the clean up at the end of the main function. Clears out the data in a
+ * ps_data struct and clears out a dlist filled with commands.
+ * 
+ * @param d ps_data struct to be cleared.
+ * @param commands commands list to be cleared.
+*/
+static void	clean_up(t_ps_data *d, t_dlist *commands)
 {
 	ft_deque_destroy_list(&d->a, free);
 	ft_deque_destroy_list(&d->b, free);
@@ -36,7 +57,18 @@ void	clean_up(t_ps_data *d, t_dlist *commands)
 	return ;
 }
 
-void	check_and_print_result(BOOL err, t_ps_data *d)
+/**
+ * @brief Checks error flag and value of the result. Prints to stdout the result
+ * 
+ * Checks the error flag and prints error response if error is true. Then if no
+ * error it checks the ps_data struct to see if it is sorted and prints the
+ * appropriate result (OK if sorted, KO if not sorted).
+ * 
+ * @param err Boolean data. True if error.
+ * @param d ps_data struct containing the state after the moves have been
+ *   applied.
+*/
+static void	check_and_print_result(BOOL err, t_ps_data *d)
 {
 	if (err == TRUE)
 	{
@@ -55,6 +87,20 @@ void	check_and_print_result(BOOL err, t_ps_data *d)
 	}
 }
 
+/**
+ * @brief Main function of the checker program
+ * 
+ * A program to check if a list of commands adequately sorts a given stack.
+ * First parses arguments to populate the stack. Checks the stack for errors and
+ * fails appropriately.
+ * 
+ * Then takes commands from stdin. Then applies these commands to the stack and
+ * prints the correct response depending on whether these commands are valid and
+ * whether they sort the stack.
+ * 
+ * @param argc Arg count.
+ * @param argv Strings containing the starting stack values.
+*/
 int	main(int argc, char **argv)
 {
 	t_ps_data	d;
--- src/checker1.c
+++ src/checker1.c
@@ -12,6 +12,17 @@
 
 #include "push_swap.h"
 
+/**
+ * @brief Applies a list of commands to the stacks in a ps_data struct
+ * 
+ * Takes a ps_data struct and sequentially applies the commands in the list of
+ * commands. If a command is not valid (ie the string does not match the subset
+ * of strings that are valid commands) then it stops and returns an error flag.
+ * 
+ * @param d ps_data struct containing stack for commands to be applied to.
+ * @param commands_head List of commands to be applied to the ps_data struct.
+ * @return Boolean flag. Returns TRUE (1) if an invalid command is found.
+*/
 int	apply_commands_to_stacks(t_ps_data *d, t_dlist *commands_head)
 {
 	t_dlist	*node;
@@ -30,6 +41,19 @@ int	apply_commands_to_stacks(t_ps_data *d, t_dlist *commands_head)
 	return (FALSE);
 }
 
+/**
+ * @brief Applies a command to the ps_data struct
+ * 
+ * Checks the string in the node passed in and if it matches a valid move it
+ * does the corresponding move to the ps_data struct (edits the stacks
+ * appropriately and appends the move to the end of the moves list).
+ * 
+ * @param d The ps_data struct to work with.
+ * @param node A dlist node which contains a string as content that may be a
+ *   valid ps command.
+ * @returns Boolean value. Returns TRUE (1) if an error occurs. Error occurs if
+ *   the string is not a valid push swap command.
+*/
 int	apply_command_to_data(t_ps_data *d, t_dlist *node)
 {
 	if (ft_strncmp(node->content, "pa\n", 4) == 0)
