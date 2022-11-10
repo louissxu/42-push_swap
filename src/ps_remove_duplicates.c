9b6f1f7b08d07fb6fb1b6415e5db4d19282c3470
Louis Xu <louis.s.xu@gmail.com>
2022-11-10 12:40:29 +1030

    feat(core): Remove redundant function and refactor ps_remove_duplicates

--- src/ps_remove_duplicates.c
+++ src/ps_remove_duplicates.c
@@ -59,43 +59,30 @@ BOOL	node_is_redundant(t_dlist *node)
 	return (FALSE);
 }
 
-// Deprecated
-t_dlist *ps_remove_skipped(t_list *l, int stack_a_length)
+static void	pop_node_and_reset_pointer(t_dlist **node, t_dlist **dlist_head)
 {
-	int	stack_b_length;
-	t_list *curr;
+	t_dlist	*prev_node;
+	t_dlist	*popped_node;
 
-	stack_b_length = 0;
-	curr = l;
-	while (curr) {
-		if (ft_strncmp(curr->content, "pb", 3) == 0)
-		{
-			if (stack_a_length == 0) {
-				// pop list element
-			} else {
-				stack_a_length--;
-				stack_b_length++;
-			}
-		}
-		else if (ft_strncmp(curr->content, "pa", 3) == 0)
-		{
-			if (stack_b_length == 0) {
-				// pop list element
-			} else {
-				stack_a_length++;
-				stack_b_length--;
-			}
-		}
+	prev_node = (*node)->prev;
+	popped_node = ft_dlist_pop_at_pointer(dlist_head, (*node)->next);
+	ft_dlist_destroy_node(popped_node, free);
+	popped_node = ft_dlist_pop_at_pointer(dlist_head, (*node));
+	ft_dlist_destroy_node(popped_node, free);
+	if (prev_node)
+	{
+		(*node) = prev_node;
 	}
-	return (NULL);
+	else
+	{
+		(*node) = (*dlist_head);
+	}
 }
 
 t_dlist	*ps_remove_duplicates(t_list *l)
 {
 	t_dlist	*dlist_head;
 	t_dlist	*node;
-	t_dlist	*prev_node;
-	t_dlist	*popped_node;
 
 	dlist_head = ft_list_to_dlist(l, clone_heap_integer_void);
 	node = dlist_head;
@@ -111,19 +98,7 @@ t_dlist	*ps_remove_duplicates(t_list *l)
 	{
 		if (node_is_redundant(node))
 		{
-			prev_node = node->prev;
-			popped_node = ft_dlist_pop_at_pointer(&dlist_head, node->next);
-			ft_dlist_destroy_node(popped_node, free);
-			popped_node = ft_dlist_pop_at_pointer(&dlist_head, node);
-			ft_dlist_destroy_node(popped_node, free);
-			if (prev_node)
-			{
-				node = prev_node;
-			}
-			else
-			{
-				node = dlist_head;
-			}
+			pop_node_and_reset_pointer(&node, &dlist_head);
 		}
 		else
 		{
