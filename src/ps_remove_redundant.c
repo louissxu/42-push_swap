da0810481f1486b1aa6a10f329f138e3bae850c8
Louis Xu <louis.s.xu@gmail.com>
2022-11-13 18:44:58 +1030

    feat(core): Initial file. Currently deprecated

---
+++ src/ps_remove_redundant.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ps_remove_redundant.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lxu <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/13 15:32:20 by lxu               #+#    #+#             */
+/*   Updated: 2022/11/13 15:32:28 by lxu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+/**
+ * @brief Remove redundant moves
+ * 
+ * Removes skipped, then removes paired moves.
+ * 
+ * DEPRECATED
+*/
+t_dlist	*ps_remove_redundant(t_ps_data data)
+{
+	return (NULL)
+}
